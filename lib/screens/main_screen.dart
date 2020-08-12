a8e09ff181084b64bf5567469c947a7f884acf38
Routos <devs.lecture28@gmail.com>
2020-08-12 10:39:52 +0700

    rechange buttom nabar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -42,116 +42,116 @@ class _MainScreenPageState extends State<MainScreenPage> {
 
   @override
   Widget build(BuildContext context) {
-    // return WillPopScope(
-    //   onWillPop: () async => false,
-    //   child: Scaffold(
-    //     bottomNavigationBar: Container(
-    //       decoration: BoxDecoration(
-    //         color: Colors.red,
-    //         borderRadius: BorderRadius.only(
-    //           topLeft: Radius.circular(10),
-    //           topRight: Radius.circular(10),
-    //         ),
-    //         boxShadow: <BoxShadow>[
-    //           BoxShadow(
-    //             color: Colors.black12,
-    //             blurRadius: 10,
-    //           ),
-    //         ],
-    //       ),
-    //       child: BottomNavigationBar(
-    //         selectedIconTheme: IconThemeData(color: kPrimaryColor),
-    //         type: BottomNavigationBarType.fixed,
-    //         elevation: 20.0,
-    //         iconSize: 24,
-    //         selectedFontSize: 13,
-    //         unselectedFontSize: 12,
-    //         selectedItemColor: kPrimaryColor,
-    //         unselectedItemColor: Color(0xFF828282),
-    //         selectedLabelStyle: TextStyle(
-    //             fontFamily: 'San-francisco', fontWeight: FontWeight.w800),
-    //         unselectedLabelStyle: TextStyle(
-    //             fontFamily: 'San-francisco', fontWeight: FontWeight.bold),
-    //         currentIndex: _pageIndex,
-    //         onTap: (index) {
-    //           setState(() {
-    //             _pageIndex = index;
-    //           });
-    //         },
-    //         items: [
-    //           BottomNavigationBarItem(
-    //             icon: Icon(
-    //               Icons.home,
-    //               size: 30,
-    //             ),
-    //             title: Text(
-    //               'HOME',
-    //             ),
-    //           ),
-    //           BottomNavigationBarItem(
-    //             icon: Icon(Icons.view_list),
-    //             title: Text(
-    //               'ORDERS',
-    //             ),
-    //           ),
-    //           BottomNavigationBarItem(
-    //             icon: Icon(
-    //               Icons.timelapse,
-    //               size: 30,
-    //             ),
-    //             title: Text(
-    //               'Pending',
-    //             ),
-    //           ),
-    //           BottomNavigationBarItem(
-    //             icon: Icon(Icons.account_circle),
-    //             title: Text(
-    //               'MY ACCOUNT',
-    //             ),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     body: _pageList[_pageIndex],
-    //   ),
-    // );
-    return MaterialApp(
-      title: 'example',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.white70,
-        bottomNavigationBar: BottomAnimation(
-          selectedIndex: _pageIndex,
-          items: items,
-          backgroundColor: Colors.white,
-          barHeight: 70,
-          itemHoverHeight: 40,
-          hoverAlignmentDuration: 400,
-          iconSize: 30,
-          onItemSelect: (value) {
-            setState(() {
-              _pageIndex = value;
-            });
-          },
-          itemHoverColor: kPrimaryColor,
-          itemHoverColorOpacity: .8,
-          activeIconColor: Colors.white,
-          deactiveIconColor: kPrimaryColor,
-          barRadius: 10,
-          textStyle: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 12,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+              ),
+            ],
           ),
-          itemHoverWidth: 120,
-          itemHoverBorderRadius: 25,
+          child: BottomNavigationBar(
+            selectedIconTheme: IconThemeData(color: kPrimaryColor),
+            type: BottomNavigationBarType.fixed,
+            elevation: 20.0,
+            iconSize: 24,
+            selectedFontSize: 13,
+            unselectedFontSize: 12,
+            selectedItemColor: kPrimaryColor,
+            unselectedItemColor: Color(0xFF828282),
+            selectedLabelStyle: TextStyle(
+                fontFamily: 'San-francisco', fontWeight: FontWeight.w800),
+            unselectedLabelStyle: TextStyle(
+                fontFamily: 'San-francisco', fontWeight: FontWeight.bold),
+            currentIndex: _pageIndex,
+            onTap: (index) {
+              setState(() {
+                _pageIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  size: 30,
+                ),
+                title: Text(
+                  'HOME',
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.view_list),
+                title: Text(
+                  'ORDERS',
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.timelapse,
+                  size: 30,
+                ),
+                title: Text(
+                  'Pending',
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                title: Text(
+                  'MY ACCOUNT',
+                ),
+              ),
+            ],
+          ),
         ),
         body: _pageList[_pageIndex],
       ),
     );
+    //   return MaterialApp(
+    //     title: 'example',
+    //     theme: ThemeData(
+    //       primarySwatch: Colors.indigo,
+    //       visualDensity: VisualDensity.adaptivePlatformDensity,
+    //     ),
+    //     home: Scaffold(
+    //       backgroundColor: Colors.white70,
+    //       bottomNavigationBar: BottomAnimation(
+    //         selectedIndex: _pageIndex,
+    //         items: items,
+    //         backgroundColor: Colors.white,
+    //         barHeight: 70,
+    //         itemHoverHeight: 40,
+    //         hoverAlignmentDuration: 400,
+    //         iconSize: 30,
+    //         onItemSelect: (value) {
+    //           setState(() {
+    //             _pageIndex = value;
+    //           });
+    //         },
+    //         itemHoverColor: kPrimaryColor,
+    //         itemHoverColorOpacity: .8,
+    //         activeIconColor: Colors.white,
+    //         deactiveIconColor: kPrimaryColor,
+    //         barRadius: 10,
+    //         textStyle: TextStyle(
+    //           color: Colors.white,
+    //           fontWeight: FontWeight.bold,
+    //           fontSize: 12,
+    //         ),
+    //         itemHoverWidth: 120,
+    //         itemHoverBorderRadius: 25,
+    //       ),
+    //       body: _pageList[_pageIndex],
+    //     ),
+    //   );
   }
 }
 
